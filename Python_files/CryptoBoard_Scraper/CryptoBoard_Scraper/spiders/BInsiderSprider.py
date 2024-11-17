fe0dabddc28bece5bcba65c2bcfa80ef734e000c
Meerza Ahmed <Ahm.Meerza@gmail.com>
2024-11-17 16:47:22 -0500

    business insider

--- Python_files/CryptoBoard_Scraper/CryptoBoard_Scraper/spiders/BInsiderSprider.py
+++ Python_files/CryptoBoard_Scraper/CryptoBoard_Scraper/spiders/BInsiderSprider.py
@@ -2,9 +2,27 @@ import scrapy
 
 
 class BinsiderspriderSpider(scrapy.Spider):
+    
     name = "BInsiderSprider"
+    
     allowed_domains = ["www.businessinsider.com"]
-    start_urls = ["https://www.businessinsider.com/"]
+    
+    start_urls = [
+        
+        "https://www.businessinsider.com/"
+    ]
 
     def parse(self, response):
-        pass
+        
+        base_url = 'https://www.businessinsider.com'
+        Tout_Titles = response.css('h3.tout-title')
+        
+        for title in Tout_Titles:
+            name = title.css('a.tout-title-link::text').get() 
+            relative_url = title.css('a.tout-title-link::attr(href)').get() 
+            full_url = base_url + relative_url
+            
+            yield {
+                'name': name, 
+                'url': full_url
+            }
