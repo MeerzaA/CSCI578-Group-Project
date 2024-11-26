223253d62e54b797decf070cf0efcfb83a9fa647
Kevin Julian Kuo <kkuo13@gmail.com>
2024-11-26 12:45:12 -0500

    Added env var for firebase credentials

--- aggregator/FireBase.py
+++ aggregator/FireBase.py
@@ -1,6 +1,9 @@
+import os
+from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db  # Use this for Realtime Database
+
 
 class FirebaseService:
     def __init__(self):
@@ -9,7 +12,11 @@ class FirebaseService:
 
     def connect_to_firebase(self):
         # Firebase initialization logic
-        cred = credentials.Certificate('/Users/bbpk/PycharmProjects/pythonProject5/crypto-board-csci578-firebase-adminsdk-srcrn-c7680a0a8d.json')
+        load_dotenv()
+        sdk_cert_path = os.getenv("ADMIN_SDK_CERT_PATH")
+        if not sdk_cert_path:
+            raise ValueError("Environment variable ADMIN_SDK_CERT_PATH is not set or accessible.")
+        cred = credentials.Certificate(sdk_cert_path)
         firebase_admin.initialize_app(cred, {"databaseURL": "https://crypto-board-csci578-default-rtdb.firebaseio.com/"})
         pass
 
@@ -30,13 +37,13 @@ class FirebaseService:
         # Logic to fetch data from Firebase
 
         #this number works. need to figure out counter per coin. replace 7 with counter var
-        ref = db.reference("Bitcoin/7")
+        ref = db.reference("Bitcoin/8")
 
         # Add a new entry without overwriting
         new_data = {
-            "Sentiment": "420.0",
-            "Site": "twitter",
-            "Source": "Social Medial",
+            "Sentiment": "421.0",
+            "Site": "tweet",
+            "Source": "Social",
             "date": "1993-05-02",
             "link": "www.twitter.com"
         }
