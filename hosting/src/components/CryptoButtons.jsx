86d3799ecc8f0604a4ed54108d3f953e9ffd8179
hazamaswag <hamzasayyid@gmail.com>
2024-12-03 18:15:01 -0800

    cleaned, sorting, styling charts

--- hosting/src/components/CryptoButtons.jsx
+++ hosting/src/components/CryptoButtons.jsx
@@ -7,14 +7,13 @@ import { getDatabase, ref, onValue } from "firebase/database";
 
 //TODO: Grab Name and sentiment from FireBase
 
-
 const CryptoButtons = () => {
-// https://samuelbankole.medium.com/google-firebase-in-react-1acc64516788
+  // https://samuelbankole.medium.com/google-firebase-in-react-1acc64516788
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const database = getDatabase(fbapp);
-    
+
     const databaseRef = ref(database);
 
     const fetchData = () => {
@@ -24,32 +23,37 @@ const CryptoButtons = () => {
         if (dataItem) {
           const item = Object.entries(dataItem);
 
-          let list_of_crypto = []
+          let list_of_crypto = [];
 
-          for (let i=0; i < item.length; i++){
-            let crypto_dates = []
-            for (let j=0; j < item[i][1].length; j++){
-              if (item[i][1][j] === undefined){
+          for (let i = 0; i < item.length; i++) {
+            let crypto_dates = [];
+            for (let j = 0; j < item[i][1].length; j++) {
+              if (item[i][1][j] === undefined) {
                 continue;
               }
               let sentiment = item[i][1][j].Sentiment;
               let date = item[i][1][j].date;
               let name = item[i][0];
 
-              crypto_dates.push({sentiment: sentiment,
+              crypto_dates.push({
+                sentiment: sentiment,
                 date: date,
-                name: name
-              })
-
+                name: name,
+              });
             }
             crypto_dates.sort((a, b) => new Date(b.date) - new Date(a.date));
             list_of_crypto.push({
               id: `${i}`,
               sentiment: crypto_dates[0].sentiment,
               date: crypto_dates[0].date,
-              crypto: <CryptoButton name={`${crypto_dates[0].name}`} sentScore={`${crypto_dates[0].sentiment}`}/>
-            })
-            
+              crypto: (
+                <CryptoButton
+                  name={`${crypto_dates[0].name}`}
+                  latest_sent_score={`${crypto_dates[0].sentiment}`}
+                  info={item[i][1]}
+                />
+              ),
+            });
           }
           setData(list_of_crypto);
         }
@@ -58,23 +62,11 @@ const CryptoButtons = () => {
     fetchData();
   }, []);
 
-//   const [data, setData] = useState([{
-//     id: "728ed52f",
-//     sentiment: 6,
-//     crypto: <CryptoButton name="Bitcoin" sentScore="6"/>,
-//   },
-// {
-//     id: "214234d",
-//     sentiment: 2,
-//     crypto: <CryptoButton name="Ethereum" sentScore="2"/>,
-//   }]);
+  return (
+    <>
+      <DataTable columns={columns} data={data} />
+    </>
+  );
+};
 
-
-return (
-  <>
-  <DataTable columns={columns} data={data} />
-  </> 
-)
-}
-
-export default CryptoButtons
+export default CryptoButtons;
